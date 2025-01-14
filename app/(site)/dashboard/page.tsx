5a5e7f4f8190e39320c107de9f269a3e49a840a5
TinyDestroyer <guptashubh.20208@gmail.com>
2025-01-14 16:23:05 +0530

    alert added

--- app/(site)/dashboard/page.tsx
+++ app/(site)/dashboard/page.tsx
@@ -96,6 +96,7 @@ const Page = (props: Props) => {
   };
 
   const queryHandler = async () => {
+    alert("shubh");
     const log = (...args : any) => console.log(JSON.stringify(args));
     log("shubh");
     console.log("hello from here");
@@ -257,7 +258,7 @@ const Page = (props: Props) => {
                 placeholder="Curious about your document? Start asking!"
                 className="h-10 px-5 text-base bg-gray-50 rounded-2xl w-3/5 text-gray-700 focus:outline-none placeholder:text-gray-400 placeholder:italic placeholder:font-sans"
               />
-              <button disabled = {query == "" ? true : false} onClick={e => {alert("aaya"); queryHandler()}}>
+              <button disabled = {query == "" ? true : false} onClick={queryHandler}>
                 <VscSend className={`transition ease-in-out duration-300 ${query == "" ? "text-gray-700" : "text-gray-400 hover:scale-110 hover:text-white"}`} />
               </button>
             </div>
