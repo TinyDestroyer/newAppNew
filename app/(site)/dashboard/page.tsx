c0c09ba4870910300397c2b94f5317aad0582280
TinyDestroyer <guptashubh.20208@gmail.com>
2025-01-14 16:02:35 +0530

    force log line

--- app/(site)/dashboard/page.tsx
+++ app/(site)/dashboard/page.tsx
@@ -96,6 +96,7 @@ const Page = (props: Props) => {
   };
 
   const queryHandler = async () => {
+    const log = (...args) => console.log(JSON.stringify(args));
     console.log("hello from here");
     try {
       setLoading(true);
